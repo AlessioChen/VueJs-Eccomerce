216a85d4f884354be9499204daa6bf1a6784ced4
alessio <alessiochen98@gmail.com>
2022-01-14 15:59:59 +0100

    add nav component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="home">
-    <Products/>
+  <div class="bg-white py-8">
+    <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
+      <Nav/>
+      <Products />
+    </div>
   </div>
 </template>
 
 <script setup>
-import Cart from '../components/Cart.vue'; 
-import Products from '../components/Products.vue'; 
-
+import Products from "../components/Products.vue";
+import Nav from "../components/Nav.vue";
 </script>
