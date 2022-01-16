4abb2ac0144188565da0c9858959506a52504da8
alessio <alessiochen98@gmail.com>
2022-01-16 12:53:28 +0100

    add remove for cart

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -15,23 +15,25 @@
     <!-- main content   -->
     <main class="mb-12">
       <ul v-for="item in cart" :key="item.id" class="p-8">
-
         <li class="flex bg-white p-5 rounded-lg shadow-lg mb-5">
           <img :src="item.image" class="w-32" />
           <div class="flex-grow flex flex-col md:flex-row items-center justify-center md:justify-between">
             <p class="title font-semibold text-sm md:text-lg mb-5 md:mb-0 md:pl-5">{{item.name}}</p>
             <div class="flex">
+              <!-- Dec Button -->
               <button class="bg-blue-200 fa fa-minus rounded-lg bg flex justify-center items-center p-3 z-10"> <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg> </button>
+                </svg>
+              </button>
               <input type="number" :value="item.quantity" class="text-center text-md font-semibold p-2 rounded w-20 focus:outline-none" />
+              <!-- Inc Button -->
               <button class="bg-blue-200 fa fa-plus rounded-lg bg flex justify-center items-center p-3 z-10">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
               </button>
-
-              <button class="bg-red-200  ml-5 fa fa-plus rounded-lg bg flex justify-center items-center p-3 z-10">
+              <!-- Remove Button -->
+              <button @click="handleRemoveButton(item) " class="bg-red-200  ml-5 fa fa-plus rounded-lg bg flex justify-center items-center p-3 z-10">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
@@ -69,13 +71,15 @@ import Nav from "../components/Nav.vue";
 const props = defineProps(["data"]);
 const emit = defineEmits(["openProduct"]);
 
-let ticket = ref({ products: null, total: 0 });
-let counter = ref(0);
-let fields = ref(["#", "remove", "image", "name", "quantity", "price"]);
 let cart = ref(JSON.parse(localStorage.getItem("cart")));
 
-const handleImageClick = (product) => {
-  emit("openProduct", product);
+const handleRemoveButton = (item) => {
+  for (let i = 0; i < cart.value.length; i++) {
+    if (cart.value[i].id == item.id) {
+      cart.value.splice(i, 1);
+      localStorage.setItem("cart", JSON.stringify(cart.value));
+    }
+  }
 };
 </script>
 
